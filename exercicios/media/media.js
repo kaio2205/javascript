37727ef87aba18dbbbf37bfb102a65adb94227b3
KAIO EDUARDO DE CARVALHO SANTOS <kaiocarvalhode@gmail.com>
2024-05-08 14:06:14 -0300

    atualizaçao

---
+++ exercicios/media/media.js
@@ -0,0 +1,33 @@
+let nota1, nota2, nota3, nota4
+ 
+ 
+function calcular() {
+    nota1 = Number(frmMedia.txtnota1.value.replace(",", "."))
+    nota2 = Number(frmMedia.txtnota2.value.replace(",", "."))
+    nota3 = Number(frmMedia.txtnota3.value.replace(",", "."))
+    nota4 = Number(frmMedia.txtnota4.value.replace(",", "."))
+ 
+    const resultado = (nota1 + nota2 + nota3 + nota4) / 4
+ 
+    if (frmMedia.txtnota1.value === "") {
+        alert("Digite  a nota")
+    } else if (frmMedia.txtnota2.value === "") {
+        alert("Digite  a nota")
+    } else if (frmMedia.txtnota3.value === "") {
+        alert("Digite  a nota")
+    } else if (frmMedia.txtnota4.value === "") {
+        alert("Digite  a nota")
+    }else {
+        if(resultado < 4 ) {
+            document.getElementById('status').src = "./img/aluno_reprovado.png"
+            alert(`A media do aluno foi ${resultado}, ele está reprovado):`)
+        }else if (resultado >= 7){
+            document.getElementById('status').src = "./img/aprovado.png"
+            alert(`A media do aluno foi ${resultado}, ele está aprovado`)
+        }else {
+            document.getElementById('status').src = "./img/recuperaçao.jpeg"
+            alert(`A media do aluno foi ${resultado}, ele está de recuperação`)
+        }
+    }
+}
+ 
